36fa6b292751e71e000b26bca29aa74a71efa1b2
xiaoxin <xiaoxin@taoshouyou.com>
2026-04-01 17:30:31 +0800

    update

--- MCurl.php
+++ MCurl.php
@@ -7,7 +7,7 @@ class MCurl
     public static int $maxTotalConnect = 20;
     public static int $sleepTime = 2;
     public static int $throttle = 100;
-    public static string $userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0';
+    public static ?string $userAgent = null;
     private array $addUrls = [];
     private Generator $rangeGenerator;
     private Generator $generator;
@@ -23,6 +23,50 @@ class MCurl
         curl_multi_setopt($this->multi, CURLMOPT_MAX_TOTAL_CONNECTIONS, self::$maxTotalConnect);
         $this->shellColumn = shell_exec('tput cols 2>/dev/null');
         $this->continue = true;
+    }
+
+    public function randomUA()
+    {
+        return mt_rand(0, 1) ? $this->firefoxUA() : $this->chromeUA();
+    }
+
+    public function firefoxUA()
+    {
+        $ri = mt_rand(0, 2);
+        $andver = mt_rand(8, 13);
+        $win = [6.1, 6.2, 10.0];
+        $winver = $win[array_rand($win)];
+        $os = ['X11', "Windows NT $winver", "Android $andver"][$ri];
+        $arch = ['Linux x86_64', 'Win64; x64', 'Mobile'][$ri];
+        $ver = mt_rand(140, 148) . '.0';
+        $geckoVer = $ri == 2 ? $ver : '20100101';
+        return "Mozilla/5.0 ($os; $arch; rv:$ver) Gecko/$geckoVer Firefox/$ver";
+    }
+
+    public function chromeUA()
+    {
+        $ri = mt_rand(0, 2);
+        $andver = mt_rand(8, 14);
+        $iosverMap = ['11.0' => 4, '12.0' => 4, '12.4' => 4, '13.0' => 5, '14.0' => 5, '15.0' => 5, '16.4' => 6, '17.0' => 6, '18.6' => 6];
+        $iosver = array_rand($iosverMap);
+        $win = [6.1, 6.2, 10.0];
+        $winver = $win[array_rand($win)];
+        $os = ['X11', "Windows NT $winver", "Linux; Android $andver", "iPhone", "Macintosh"][$ri];
+        $macVer = ['26_2_0', '15_7.2', '14_8_2', '12_7_8', '13_6_7'];
+        $macVer = $macVer[array_rand($macVer)];
+        $arch = ['Linux x86_64', 'Win64; x64', 'Andorid', "CPU iPhone OS $iosver like Mac OS X", "Intel Mac OS X $macVer"][$ri];
+        $ver = '131.0.0.0';
+        $mobile = $ri == 2 ? 'Mobile' : '';
+        $chrome = 'Chrome';
+        if ($ri == 3) {
+            $mobile = 'Mobile/15E148';
+            $chrome = 'CriOS';
+        }
+        $webkitVer = '537.36';
+        if ($ri == 3) {
+            $webkitVer = '60' . $iosverMap[$iosver] . '.1';
+        }
+        return "Mozilla/5.0 ($os; $arch) AppleWebKit/$webkitVer (KHTML, like Gecko) $chrome/$ver $mobile Safari/$webkitVer";
     }
 
     public function error($url, string $error)
@@ -77,7 +121,7 @@ class MCurl
         $this->changeState('add', $url, 0);
         $ch = curl_init($url);
         curl_setopt_array($ch, [
-            CURLOPT_USERAGENT => self::$userAgent,
+            CURLOPT_USERAGENT => self::$userAgent ?? $this->randomUA(),
             CURLOPT_RETURNTRANSFER => true,
         ]);
         if (($mr = curl_multi_add_handle($this->multi, $ch)) !== CURLM_OK) {
